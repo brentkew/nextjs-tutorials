d8aece5d109be430635e762aeb4204f97dc90cf2
Khalil Ahmad <brentkew@gmail.com>
2024-08-03 20:42:19 +0500

    Adding Register Form

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <div>
+      <form action="">
+        <input type="text" name="username" placeholder="Username" />
+        <input type="email" name="email" placeholder="Email" />
+        <input type="password" name="password" placeholder="Password" />
+        <input type="password" name="passwordAgain" placeholder="Enter password again" />
+        <button>Register</button>
+      </form>
+    </div>
   )
 }
 
